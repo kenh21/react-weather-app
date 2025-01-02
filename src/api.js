a17cf6795a97a6f6f639cec4d60250d5d5854de6
Ken Hauter <kenneth.hauter@gmail.com>
2025-01-02 18:16:19 +0900

    Add fetch to currentWeather
    added an api fetch to get current weather for the
    current weather component.

--- src/api.js
+++ src/api.js
@@ -7,3 +7,5 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities`;
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5785b526335dfdf026cbfc61d52e88e9";
