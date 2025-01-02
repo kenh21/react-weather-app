06c2373ed80597ce861ca3ff6f999f65661bead2
Ken Hauter <kenneth.hauter@gmail.com>
2025-01-02 16:39:19 +0900

    Add search component
    added a search component using the geoApi

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '4142a25409msh3b91e00433e3dcbp153d1djsnc87fc63bd41b',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities`;
